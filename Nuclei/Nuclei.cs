d2ebf4d61bafbc8277ae36c7d2e61dd41cef98c1
MaxWasUnavailable <89798523+MaxWasUnavailable@users.noreply.github.com>
2025-02-06 21:20:12 +0100

    Initial commit

---
+++ Nuclei/Nuclei.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace Nuclei;
+
+/// <summary>
+///     Main plugin class for Tempest.
+/// </summary>
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Nuclei : BaseUnityPlugin
+{
+    private new static ManualLogSource? Logger { get; set; }
+    internal static Nuclei? Instance { get; private set; }
+    private static Harmony? Harmony { get; set; }
+    private static bool IsPatched { get; set; }
+
+    private void Awake()
+    {
+        Instance = this;
+
+        // Init logger
+        Logger = base.Logger;
+
+        Harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+
+        PatchAll();
+
+        // Report plugin loaded
+        if (IsPatched)
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        else
+            Logger.LogError($"Plugin {PluginInfo.PLUGIN_GUID} failed to load correctly!");
+    }
+
+    private void PatchAll()
+    {
+        if (IsPatched)
+        {
+            Logger?.LogWarning("Already patched!");
+            return;
+        }
+
+        Logger?.LogDebug("Patching...");
+
+        Harmony ??= new Harmony(PluginInfo.PLUGIN_GUID);
+
+        try
+        {
+            Harmony.PatchAll();
+            IsPatched = true;
+            Logger?.LogDebug("Patched!");
+        }
+        catch (Exception e)
+        {
+            Logger?.LogError($"Failed to patch: {e}");
+        }
+    }
+
+    private void UnpatchSelf()
+    {
+        if (!IsPatched)
+        {
+            Logger?.LogWarning("Already unpatched!");
+            return;
+        }
+
+        Logger?.LogDebug("Unpatching...");
+
+        Harmony?.UnpatchSelf();
+        IsPatched = false;
+
+        Logger?.LogDebug("Unpatched!");
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Nuclei
+
+Nuclei is a dedicated server solution for Nuclear Option. It is designed to offer a flexible, easy-to-use, and
+easy-to-modify server solution.
+
+Plugins (essentially server-only mods) are first-class citizens in Nuclei, and are designed to be easy to create and
+use. This offers a wide range of possibilities for server owners to customize their server to their liking.
