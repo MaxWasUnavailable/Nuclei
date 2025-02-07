c6006b805291727d808dddecd0cab1d9c1a636c2
MaxWasUnavailable <89798523+MaxWasUnavailable@users.noreply.github.com>
2025-02-07 03:49:44 +0100

    Create Globals helper

---
+++ Nuclei/Helpers/Globals.cs
@@ -0,0 +1,30 @@
+using System;
+using NuclearOption.Networking;
+
+namespace Nuclei.Helpers;
+
+/// <summary>
+///     Has property getters for various Nuclear Option values that are used throughout the mod.
+/// </summary>
+public static class Globals
+{
+    /// <summary>
+    ///     Gets the instance of the <see cref="NetworkManagerNuclearOption" /> class.
+    /// </summary>
+    public static NetworkManagerNuclearOption NetworkManagerNuclearOptionInstance => NetworkManagerNuclearOption.i ?? throw new NullReferenceException("NetworkManagerNuclearOption instance is null.");
+
+    /// <summary>
+    ///     Gets the instance of the <see cref="MissionManager" /> class.
+    /// </summary>
+    public static MissionManager MissionManagerInstance => MissionManager.i ?? throw new NullReferenceException("MissionManager instance is null.");
+
+    /// <summary>
+    ///     Gets the local player. (The host / server)
+    /// </summary>
+    public static Player LocalPlayer => GameManager.LocalPlayer ?? throw new NullReferenceException("Local player is null.");
+
+    /// <summary>
+    ///     Gets the name of the local player. (The host / server)
+    /// </summary>
+    public static string HostName => LocalPlayer.PlayerName;
+}
