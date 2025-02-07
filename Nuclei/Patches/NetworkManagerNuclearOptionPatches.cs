eed64d9e457c3eeed02a084dbf82a3c4c2ffeb21
MaxWasUnavailable <89798523+MaxWasUnavailable@users.noreply.github.com>
2025-02-07 03:49:56 +0100

    Add placeholder patch

---
+++ Nuclei/Patches/NetworkManagerNuclearOptionPatches.cs
@@ -0,0 +1,7 @@
+namespace Nuclei.Patches;
+
+// TODO: Patch the start method with a postfix to start the server right after the network manager is ready.
+public class NetworkManagerNuclearOptionPatches
+{
+    
+}
