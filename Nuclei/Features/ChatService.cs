dc27a1523aec5afba06f30eb4161d947f7e17990
MaxWasUnavailable <89798523+MaxWasUnavailable@users.noreply.github.com>
2025-02-11 22:22:50 +0100

    Add chat message variable filling

--- Nuclei/Features/ChatService.cs
+++ Nuclei/Features/ChatService.cs
@@ -1,3 +1,4 @@
+using System;
 using Nuclei.Helpers;
 
 namespace Nuclei.Features;
@@ -19,14 +20,40 @@ public static class ChatService
             Nuclei.Logger?.LogWarning("Chat manager instance is null.");
             return false;
         }
-        return ChatManager.CanSend(message, true, true);
+
+        try
+        {
+            return ChatManager.CanSend(message, true, true);
+        }
+        catch (ArgumentException e)
+        {
+            Nuclei.Logger?.LogError($"Cannot send chat message: {e.Message}");
+            return false;
+        }
+    }
+    
+    /// <summary>
+    ///     Fills chat message variables.
+    /// </summary>
+    /// <param name="message"> The message to fill. </param>
+    /// <param name="player"> The player to use for filling player-specific variables. </param>
+    /// <returns></returns>
+    public static string FillChatMessageVariables(string message, Player? player = null)
+    {
+        if (player != null)
+        {
+            message = message.Replace("{username}", player.PlayerName);
+        }
+        
+        return message;
     }
 
     /// <summary>
     ///     Sends a chat message to all clients.
     /// </summary>
     /// <param name="message"> The message to send. </param>
-    public static void SendChatMessage(string message)
+    /// <param name="player"> The player sending the message. </param>
+    public static void SendChatMessage(string message, Player? player = null)
     {
         if (!CanSend(message))
         {
@@ -34,6 +61,6 @@ public static class ChatService
             return;
         }
         
-        Globals.ChatManagerInstance.CmdSendChatMessage(message, true);
+        Globals.ChatManagerInstance.CmdSendChatMessage(FillChatMessageVariables(message, player), true);
     }
 }
