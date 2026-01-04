6d6cce7eec54798fd3abb5fad98b4a8ae256c864
James Armstrong <jm.armstrong1207@gmail.com>
2026-01-04 04:15:39 -0800

    Fix PlayerUtils.cs

--- Nuclei/Helpers/PlayerUtils.cs
+++ Nuclei/Helpers/PlayerUtils.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Mirage;
 using NuclearOption.Networking;
+using Nuclei.Features;
 
 namespace Nuclei.Helpers;
 
@@ -10,26 +12,15 @@ namespace Nuclei.Helpers;
 /// </summary>
 public static class PlayerUtils
 {
-    /// <summary>
-    ///     Get the Player object from an INetworkPlayer object.
-    /// </summary>
-    /// <param name="networkPlayer"> The INetworkPlayer object. </param>
-    /// <returns> The Player object, if available. </returns>
-    public static Player? GetPlayer(this INetworkPlayer networkPlayer)
-    {
-        return networkPlayer.Identity?.GetComponent<Player>();
-    }
 
     /// <summary>
     ///     Get the Player object from an INetworkPlayer object, if available.
     /// </summary>
     /// <param name="networkPlayer"> The INetworkPlayer object. </param>
-    /// <param name="playerComponent"> The Player component, if available. </param>
     /// <returns> The Player object, if available. </returns>
-    public static bool TryGetPlayer(this INetworkPlayer networkPlayer, out Player? playerComponent)
+    public static bool TryGetPlayer(this INetworkPlayer networkPlayer, out Player? player)
     {
-        playerComponent = networkPlayer.GetPlayer();
-        return playerComponent != null;
+        return PlayerHelper.TryGetPlayer(networkPlayer, out player);
     }
 
     /// <summary>
@@ -40,7 +31,42 @@ public static class PlayerUtils
     /// <returns></returns>
     public static bool TryFindPlayer(string playerName, out Player? playerObject)
     {
-            playerObject = Globals.AuthenticatedPlayers.FirstOrDefault(p => StripStaffPrefix(p.GetPlayer()?.PlayerName ?? "").ToLower().StartsWith(StripStaffPrefix(playerName).ToLower()))?.GetPlayer();
-        return playerObject != null;
+            return TryGetPlayer(Globals.AuthenticatedPlayers.FirstOrDefault(p =>
+            {
+                Player po;
+                TryGetPlayer(p, out po);
+                return StripStaffPrefix(po.PlayerName ?? "").ToLower()
+                    .StartsWith(StripStaffPrefix(playerName).ToLower());
+            }), out playerObject);
+    }
+    
+    /// <summary>
+    ///     Utility function to strip a player name of the staff tag, if they have it.
+    /// </summary>
+    /// <param name="playerName"> The player name. </param>
+    /// <returns>Actual playername.</returns>
+    public static string StripStaffPrefix(string playerName)
+    {
+        if (string.IsNullOrEmpty(playerName))
+            return playerName;
+
+        var pattern = $@"^{Regex.Escape(NucleiConfig.StaffPrefix!.Value)}\s*";
+        var cleanName = Regex.Replace(playerName, pattern, "", RegexOptions.IgnoreCase);
+
+        return cleanName;
+    }
+
+    /// <summary>
+    ///     Apply or remove the staff tag based on player permission level.
+    /// </summary>
+    /// <param name="playerObject"> The Player object. </param>
+    /// <returns></returns>
+    public static void ApplyOrRemoveStaffTag(Player playerObject)
+    {
+        if (!NucleiConfig.UseStaffPrefix!.Value || (!NucleiConfig.IsAdmin(playerObject.SteamID) &&
+                                                   !NucleiConfig.IsOwner(playerObject.SteamID) &&
+                                                   !NucleiConfig.IsModerator(playerObject.SteamID))) return;
+        var newName = $"{NucleiConfig.StaffPrefix!.Value} {playerObject.PlayerName}";
+        playerObject.PlayerName = newName;
     }
 }
