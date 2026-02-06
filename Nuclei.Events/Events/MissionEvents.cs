8f86bdf6a72a3f9dde39a06d4755e12160c4ea87
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2026-02-07 00:53:25 +0100

    Remove MissionEnded events

--- Nuclei.Events/Events/MissionEvents.cs
+++ Nuclei.Events/Events/MissionEvents.cs
@@ -18,16 +18,6 @@ public static class MissionEvents
     public static event Action? PostMissionStarted;
 
     /// <summary>
-    ///     Event handler fired before a mission ends.
-    /// </summary>
-    public static event Action? PreMissionEnded;
-
-    /// <summary>
-    ///     Event handler fired after a mission ends.
-    /// </summary>
-    public static event Action? PostMissionEnded;
-
-    /// <summary>
     ///     Event handler fired before a mission completes.
     /// </summary>
     public static event Action? PreMissionCompleted;
@@ -45,16 +35,6 @@ public static class MissionEvents
     internal static void OnPostMissionStarted()
     {
         PostMissionStarted?.Invoke();
-    }
-
-    internal static void OnPreMissionEnded()
-    {
-        PreMissionEnded?.Invoke();
-    }
-
-    internal static void OnPostMissionEnded()
-    {
-        PostMissionEnded?.Invoke();
     }
 
     internal static void OnPreMissionCompleted()
