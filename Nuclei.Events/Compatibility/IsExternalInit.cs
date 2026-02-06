1c3da1c7f9c01a5d6827e641e531cc5a915df57b
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2026-02-06 23:06:13 +0100

    Add polyfill to Nuclei.Events

---
+++ Nuclei.Events/Compatibility/IsExternalInit.cs
@@ -0,0 +1,11 @@
+﻿// ReSharper disable UnusedType.Global
+// ReSharper disable once CheckNamespace
+namespace System.Runtime.CompilerServices;
+
+/// <summary>
+///     Polyfill for C# 9 init-only features when targeting net48.
+/// </summary>
+internal static class IsExternalInit
+{
+}
+
