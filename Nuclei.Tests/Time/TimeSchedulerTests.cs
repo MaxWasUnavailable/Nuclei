066084397ce228dce12ed5a58fe11ef5afe6d579
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2026-02-07 00:59:40 +0100

    Add TimeScheduler tests

---
+++ Nuclei.Tests/Time/TimeSchedulerTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using FluentAssertions;
+using Nuclei.Core.Time;
+using Nuclei.Events.Events;
+
+namespace Nuclei.Tests.Time;
+
+/// <summary>
+///     Tests for <see cref="TimeScheduler" />.
+/// </summary>
+public sealed class TimeSchedulerTests
+{
+    [Test]
+    public void Tick_WithSubSecondDelta_DoesNotFireEverySecond()
+    {
+        var scheduler = new TimeScheduler();
+        var secondsFired = 0;
+
+        TimeEvents.EverySecond += Handler;
+        try
+        {
+            scheduler.Tick(TimeSpan.FromMilliseconds(500));
+        }
+        finally
+        {
+            TimeEvents.EverySecond -= Handler;
+        }
+
+        secondsFired.Should().Be(0);
+        return;
+
+        void Handler() => secondsFired++;
+    }
+
+    [Test]
+    public void Tick_WithMultipleSeconds_FiresEverySecondAndIntervalCallbacks()
+    {
+        var scheduler = new TimeScheduler();
+        var secondsFired = 0;
+        var intervalFired = 0;
+
+        TimeEvents.EverySecond += Handler;
+        try
+        {
+            using var registration = scheduler.RegisterInterval(TimeSpan.FromSeconds(2), () => intervalFired++);
+            scheduler.Tick(TimeSpan.FromSeconds(5));
+        }
+        finally
+        {
+            TimeEvents.EverySecond -= Handler;
+        }
+
+        secondsFired.Should().Be(5);
+        intervalFired.Should().Be(2);
+        return;
+
+        void Handler() => secondsFired++;
+    }
+
+    [Test]
+    public void RegisterInterval_DisposeStopsFurtherCallbacks()
+    {
+        var scheduler = new TimeScheduler();
+        var intervalFired = 0;
+
+        var registration = scheduler.RegisterInterval(TimeSpan.FromSeconds(2), () => intervalFired++);
+        scheduler.Tick(TimeSpan.FromSeconds(2));
+        registration.Dispose();
+        scheduler.Tick(TimeSpan.FromSeconds(2));
+
+        intervalFired.Should().Be(1);
+    }
+
+    [Test]
+    public void RegisterInterval_WithTooSmallInterval_Throws()
+    {
+        var scheduler = new TimeScheduler();
+
+        var act = () => scheduler.RegisterInterval(TimeSpan.FromMilliseconds(500), () => { });
+
+        act.Should().Throw<ArgumentOutOfRangeException>();
+    }
+
+    [Test]
+    public void RegisterInterval_WithNullCallback_Throws()
+    {
+        var scheduler = new TimeScheduler();
+
+        var act = () => scheduler.RegisterInterval(TimeSpan.FromSeconds(1), null!);
+
+        act.Should().Throw<ArgumentNullException>();
+    }
+}
+
