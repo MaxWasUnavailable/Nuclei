55ebc080fe354934aa81de91567590a825b4a45c
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2026-02-07 00:41:31 +0100

    Add Dedicated Server Manager Patches

---
+++ Nuclei.Patches/DedicatedServerManagerPatches.cs
@@ -0,0 +1,43 @@
+﻿using HarmonyLib;
+using NuclearOption.DedicatedServer;
+using NuclearOption.SavedMission;
+using Nuclei.Events.Events;
+
+namespace Nuclei.Patches;
+
+[HarmonyPatch(typeof(DedicatedServerManager))]
+[HarmonyPriority(Priority.First)]
+[HarmonyWrapSafe]
+internal static class DedicatedServerManagerPatches
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(DedicatedServerManager.StartServer))]
+    private static void StartServerPrefix()
+    {
+        ServerEvents.OnPreServerStarted();
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(DedicatedServerManager.StartServer))]
+    private static void StartServerPostfix()
+    {
+        ServerEvents.OnPostServerStarted();
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(DedicatedServerManager.LoadMissionMap))]
+    [HarmonyPatch([typeof(Mission)])]
+    private static void LoadMissionMapPrefix()
+    {
+        MissionEvents.OnPreMissionStarted();
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(DedicatedServerManager.LoadMissionMap))]
+    [HarmonyPatch([typeof(Mission)])]
+    private static void LoadMissionMapPostfix()
+    {
+        MissionEvents.OnPostMissionStarted();
+    }
+}
+
