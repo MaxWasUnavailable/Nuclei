3f491f011922703202a17cbfbc0926247aeaf611
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2026-02-06 23:05:56 +0100

    Add ChatMessage abstraction

---
+++ Nuclei.Abstractions/Chat/ChatMessage.cs
@@ -0,0 +1,9 @@
+﻿using Nuclei.Abstractions.Players;
+
+namespace Nuclei.Abstractions.Chat;
+
+/// <summary>
+///     Data for a chat message event.
+/// </summary>
+public sealed record ChatMessage(IPlayerInfo Sender, string Message, bool AllChat);
+
