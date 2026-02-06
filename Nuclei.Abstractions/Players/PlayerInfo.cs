8c213f18bc6c599cf681595963cbbdb6e505b414
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2026-02-06 20:51:50 +0100

    Add PlayerInfo record

---
+++ Nuclei.Abstractions/Players/PlayerInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Nuclei.Abstractions.Players;
+
+/// <summary>
+///     Default implementation of <see cref="IPlayerInfo"/>.
+/// </summary>
+/// <param name="Name">The player's name.</param>
+/// <param name="SteamId">The player's Steam ID.</param>
+public sealed record PlayerInfo(string? Name, ulong SteamId) : IPlayerInfo;
