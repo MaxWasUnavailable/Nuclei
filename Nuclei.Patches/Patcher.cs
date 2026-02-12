d0c417dacda1235afef3e04c1bdc362a90adef32
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2026-02-12 02:29:32 +0100

    Make patcher more robust to game updates
    Should no longer cause all patching to fail if only some classes have changed

--- Nuclei.Patches/Patcher.cs
+++ Nuclei.Patches/Patcher.cs
@@ -1,4 +1,8 @@
-﻿using HarmonyLib;
+﻿using System;
+using System.Linq;
+using HarmonyLib;
+using Nuclei.Abstractions.BepInEx.Logging;
+using Nuclei.Abstractions.Nuclei.Decorators;
 
 namespace Nuclei.Patches;
 
@@ -10,8 +14,32 @@ internal static class Patcher
     /// <summary>
     ///     Applies all patches in the assembly.
     /// </summary>
-    public static void ApplyPatches(Harmony harmony)
+    public static void ApplyPatches(Harmony harmony, ILogger logger)
     {
-        harmony.PatchAll();
+        var patchLogger = logger.WithScope(nameof(Patcher));
+
+        var patchTypes = typeof(Patcher).Assembly
+            .GetTypes()
+            .Where(t => t.GetCustomAttributes(typeof(HarmonyPatch), true).Length > 0)
+            .ToArray();
+
+        var errorCount = 0;
+
+        foreach (var patchType in patchTypes)
+            try
+            {
+                var processor = new PatchClassProcessor(harmony, patchType);
+                processor.Patch();
+                patchLogger.Debug($"Successfully applied Harmony patches in: {patchType.FullName}");
+            }
+            catch (Exception e)
+            {
+                errorCount++;
+                patchLogger.Warn($"Failed to apply Harmony patch: {patchType.FullName}");
+                patchLogger.Error("Harmony patch failure", e);
+            }
+
+        if (errorCount > 0)
+            throw new Exception($"Failed to apply {errorCount} Harmony patches.");
     }
 }
