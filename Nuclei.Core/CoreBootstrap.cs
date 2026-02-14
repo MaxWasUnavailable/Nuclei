977a24c9954cf176530656338188a3bab79fd153
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2026-02-14 01:11:53 +0100

    Create Bootstrap for Nuclei's core

---
+++ Nuclei.Core/CoreBootstrap.cs
@@ -0,0 +1,58 @@
+﻿using Nuclei.Abstractions.BepInEx.Config;
+using Nuclei.Abstractions.BepInEx.Logging;
+using Nuclei.Abstractions.Nuclei;
+using Nuclei.Abstractions.Nuclei.Decorators;
+using Nuclei.Core.Config;
+using Nuclei.Core.Services;
+using IServiceProvider = Nuclei.Abstractions.Nuclei.IServiceProvider;
+
+namespace Nuclei.Core;
+
+/// <summary>
+///     Static class responsible for initializing Nuclei's core components, such as the context, services, and other shared
+///     resources.
+/// </summary>
+public static class CoreBootstrap
+{
+    /// <summary>
+    ///     Initializes Nuclei's context & services.
+    /// </summary>
+    public static INucleiContext Initialize(ILogger logger, IConfigProvider config)
+    {
+        var bootstrapLogger = logger.WithTimestamp().WithScope(nameof(Core));
+
+        bootstrapLogger.Debug("Starting Nuclei bootstrap process...");
+
+        bootstrapLogger.Debug("Initializing Nuclei config.");
+        NucleiConfig.Initialize(config, logger); // TODO: rework config stuff
+        NucleiConfig.ValidateSettings();
+        bootstrapLogger.Debug("Nuclei config initialized successfully.");
+
+        bootstrapLogger.Debug("Initializing Nuclei context.");
+        var context = InitializeContext(config, logger);
+        bootstrapLogger.Debug("Nuclei context initialized successfully.");
+
+        bootstrapLogger.Info("Core components initialized successfully.");
+
+        return context;
+    }
+
+    private static IServiceProvider BuildServiceProvider()
+    {
+        var serviceRegistry = new ServiceRegistry();
+
+        serviceRegistry.Register(new TimeSchedulerService());
+
+        return serviceRegistry;
+    }
+
+
+    private static INucleiContext InitializeContext(IConfigProvider config, ILogger logger)
+    {
+        var context = new NucleiContext(BuildServiceProvider(), logger, config);
+
+        context.InitializeServices();
+
+        return context;
+    }
+}
