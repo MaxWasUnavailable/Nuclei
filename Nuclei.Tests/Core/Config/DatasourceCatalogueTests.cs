4c057dcdc3e2d6060d4010eb4ecfac76f02c8377
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2026-02-17 00:11:37 +0100

    Add datasource catalogue tests

---
+++ Nuclei.Tests/Core/Config/DatasourceCatalogueTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Nuclei.Core.Config.Datasource;
+
+namespace Nuclei.Tests.Core.Config;
+
+public sealed class DatasourceCatalogueTests
+{
+    [Test]
+    public void GetBinding_WithUnknownName_FallsBackToDefault()
+    {
+        var bindings = new Dictionary<string, DatasourceBindingConfig>
+        {
+            ["*"] = new("*", ["default"], ["default"])
+        };
+
+        var sources = new Dictionary<string, DatasourceConfig>
+        {
+            ["default"] = new("default", "sqlite://nuclei.db")
+        };
+
+        var catalogue = new DatasourceCatalogue(bindings, sources, "*");
+
+        var result = catalogue.GetBinding("unknown");
+
+        result.Should().Be(bindings["*"]);
+    }
+
+    [Test]
+    public void GetSources_WithUnknownName_FallsBackToDefault()
+    {
+        var bindings = new Dictionary<string, DatasourceBindingConfig>
+        {
+            ["*"] = new("*", ["default"], ["default"])
+        };
+
+        var sources = new Dictionary<string, DatasourceConfig>
+        {
+            ["default"] = new("default", "sqlite://nuclei.db")
+        };
+
+        var catalogue = new DatasourceCatalogue(bindings, sources, "*");
+
+        var result = catalogue.GetAllReadSourcesForBinding("unknown");
+
+        result.Should().HaveCount(1);
+        result.Should().Contain(sources["default"]);
+    }
+}
+
